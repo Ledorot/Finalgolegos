8b7ccf56f1bc1a7a19baaf455e65d7351bd4b0cb
Goncalo Marques <goncalo.cool@gmail.com>
2017-01-03 19:34:52 +0000

    Readded MapCharacter and PlayerController

---
+++ Golegos/Assets/Scripts/MapCharacter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+public class MapCharacter : MonoBehaviour {
+
+    //Character components
+    private Rigidbody2D rb;
+
+    //Movement variables
+    [SerializeField]
+    private float movSpeed = 6f;
+
+    /****************
+    *   METHODS
+    ****************/
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    //Handles the character's movement
+    public void Move(float moveX, float moveY) {
+
+        rb.velocity = new Vector3(moveX * movSpeed, moveY * movSpeed, 0f);
+    }
+}
---
+++ Golegos/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MapCharacter))]
+public class PlayerController : MonoBehaviour {
+
+    //Character reference
+    private MapCharacter character;
+
+    /******************
+    *   METHODS
+    ******************/
+
+
+    private void Awake() {
+        character = GetComponent<MapCharacter>();
+    }
+
+    private void FixedUpdate() {
+
+        //Player's horizontal movement
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        character.Move(h, v);
+    }
+}
